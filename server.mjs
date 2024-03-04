6e2cd9c3073aa6cb2d20ce873ab547efd6d3c73e
T-A-U <153134801+T-A-U@users.noreply.github.com>
2024-03-03 19:12:03 -0700

    Initial Structure

--- server.mjs
+++ server.mjs
@@ -16,17 +16,44 @@
 
 // Boiler plate for express application, will connect to database
 
-import express from 'express'
+import express from 'express';
+import mongoose from "mongoose";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Replace the uri string with your connection string.
+const mongoURI = process.env.MONGO_URI
+await mongoose.connect(mongoURI);
 
 const app = express()
 
 let PORT = 3000 
+//Create a schema, move to separate file eventually - code from mongoose documentation
+const gamesSchema = new mongoose.Schema({
+    name: String  
+  });
+const GamesModel = mongoose.model('Game', gamesSchema, "GTAgames");
 
 
-app.get('/',(req,res) =>{
-    res.send('hello, World')
+app.get('/',async (req,res) =>{
+    // res.send('hello, World')
+    const games = await GamesModel.find({});
+    console.log("All games,",games);
+    res.send(games)
 })
 
+app.post('/games', async (req,res)=> {
+    const game = new GamesModel({ name: 'KingdomHearts' });
+    console.log(game.name); // '1080 Avalanche'
+    await game.save();
+})
+
+app.delete('games/:_id', async (req,res)=> {
+    //does not work yet
+    const game = await GamesModel.findByIdAndDelete(_id) 
+})
 
 app.listen(PORT, ()=>{
     console.log(`server is listening on: ${PORT}`)
